5180da2abf3dc3acdcdbee34d43b522c12653ddc
thoma <thomas.t.vanderlindeN@student.hu.nl>
2022-02-08 11:57:02 +0100

    Initial commit

---
+++ src/hallo.java
@@ -0,0 +1,27 @@
+class halloJava {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("de gegevens van nr 1 zijn:");
+        String naam1 = "jan";
+        String adres1 = "Nijenoord 1";
+        String plaats1 = "Utrecht";
+
+        System.out.println(naam1);
+        System.out.println(adres1);
+        System.out.println(plaats1);
+
+        System.out.println("de gegevens van nr 2 zijn:");
+        String naam2 = "Wim";
+        String adres2 = "Oudenoord 340";
+        String plaats2 = "Utrecht";
+
+        System.out.println(naam2);
+        System.out.println(adres2);
+        System.out.println(plaats2);
+    }
+}
+
+
+
---
+++ src/practicum1.java
@@ -0,0 +1,5 @@
+public class practicum1 {
+    public static void main(String[] args) {
+
+    }
+}
